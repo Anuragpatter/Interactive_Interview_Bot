23722133cef6e1e673521a60170e37080f030134
Anurag patter <93168851+Anuragpatter@users.noreply.github.com>
2023-05-12 12:44:25 +0530

    Add files via upload

---
+++ IIB/djangoapp/urls.py
@@ -0,0 +1,47 @@
+"""djangoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin  
+from django.urls import path  
+from myapp import views as user_views
+from admin_app import views as admin_views
+urlpatterns = [  
+    path('', user_views.login),
+    path('login/',user_views.login),
+    path('admin/', admin.site.urls),  
+    path('demo/', user_views.demo),  
+    path('home/', user_views.home),
+    path('features/', user_views.features),
+    path('integration/', user_views.integration),
+    path('resources/', user_views.resources),
+    path('solutions/', user_views.solutions),
+    path('registration/', user_views.registration),
+    path('result/', user_views.result),
+    path('dashboard/', user_views.dashboard),
+    path('admin_login/',admin_views.admin_login),
+    path('jobs/', user_views.jobs),
+
+    path('admin_demo/', admin_views.admin_demo),  
+    path('admin_home/', admin_views.admin_home),
+    path('admin_features/', admin_views.admin_features),
+    path('admin_integration/', admin_views.admin_integration),
+    path('admin_resources/', admin_views.admin_resources),
+    path('admin_solutions/', admin_views.admin_solutions),
+    path('admin_registration/', admin_views.admin_registration),
+    path('admin_result/', admin_views.admin_result),
+    path('admin_dashboard/', admin_views.admin_dashboard),
+    path('new_job/', admin_views.new_job)
+
+]  
---
+++ IIB/myapp/views.py
@@ -0,0 +1,179 @@
+from django.shortcuts import render
+#User_app
+from django.http import HttpResponse  
+from myapp.functions.functions import handle_uploaded_file  
+from myapp.forms import StudentForm  
+
+import mysql.connector as sql
+fn=''
+ln=''
+mail=''
+pw=''
+cpw=''
+id = 0
+
+resumeScore=''
+job=''
+
+
+def demo(request):  
+    if request.method == 'POST':  
+        global fn,ln,mail,job,resumeScore
+        student = StudentForm(request.POST, request.FILES) 
+        context={}
+        score = handle_uploaded_file(request.FILES['file'],request.POST['job'])
+        # context = {
+        #     'score': score,
+        #     'name': request.POST['firstname'],
+        #     'job' : request.POST['job']
+        # }
+        m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+        cursor = m.cursor()
+        data = request.POST
+        for key,value in data.items():
+            if key=="firstname":
+                fn = value
+            if key =="lastname":
+                ln = value
+            if key=="email":
+                mail = value
+            if key =="job":
+                job = value
+        
+
+        cmd = "insert into resume Values('{}','{}','{}','{}','{}')".format(fn,ln,mail,job,score)
+        cursor.execute(cmd)
+        m.commit()
+
+        cmd = "select * from resume"
+        cursor.execute(cmd)
+        t = tuple(cursor.fetchall())
+        context = {
+            'score': score,
+            'name': request.POST['firstname'],
+            'job' : request.POST['job'],
+            'resumedata' : t
+        }
+        return render(request,"result.html",context)
+        
+        # if student.is_valid():  
+        #     handle_uploaded_file(request.FILES['file'])  
+        #     # return HttpResponse("File uploaded successfuly")  
+        #     score = num
+        #     return render(request,"result.html",score)
+    else:  
+        student = StudentForm()  
+        return render(request,"demo.html",{'form':student})  
+
+def login(request):
+    global mail,pw,id
+    if request.method == 'POST':
+        m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+        cursor = m.cursor()
+        data = request.POST
+        for key,value in data.items():
+            if key=="email":
+                mail = value
+            if key =="password":
+                pw = value
+
+        cmd = "select * from users where email='{}' and password='{}'".format(mail,pw)
+        cursor.execute(cmd)
+        t = tuple(cursor.fetchall())
+        if t==():
+            context = {
+                'msg': 'wrong email or password'
+            }
+            return render(request,'login.html',context)
+        else:
+            cmd = "select UserID from users where email='{}'".format(mail)
+            cursor.execute(cmd)
+            t = tuple(cursor.fetchall())
+            id = t[0][0]
+            return render(request,'home.html')
+
+    else:
+        return render(request,"login.html")
+
+def home(request):
+    return render(request,"home.html")
+
+def features(request):
+    return render(request,"features.html")
+
+def integration(request):
+    return render(request,"integration.html")
+
+def resources(request):
+    return render(request,"resources.html")
+
+def solutions(request):
+    return render(request,"solutions.html")
+
+def registration(request):
+    global mail,pw 
+    global fn,ln,mail,pw,cpw 
+    if request.method == 'POST':
+        m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+        cursor = m.cursor()
+        data = request.POST
+        for key,value in data.items():
+            if key=="firstname":
+                fn = value
+            if key =="lastname":
+                ln = value
+            if key=="email":
+                mail = value
+            if key =="password":
+                pw = value
+            if key =="cpassword":
+                cpw = value
+
+        cmd = "insert into users(FirstName,LastName,Email,Password,cPassword) Values('{}','{}','{}','{}','{}')".format(fn,ln,mail,pw,cpw)
+        cursor.execute(cmd)
+        m.commit()
+        context = {
+            'msg': 'Sucessfully registered'
+        }
+        return render(request,'signup.html',context)
+
+    else:
+        return render(request,"signup.html")
+
+def result(request):
+    m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+    cursor = m.cursor()
+    
+    cmd = "select * from users"
+    cursor.execute(cmd)
+    t = tuple(cursor.fetchall())
+    context = {
+        'data' : t
+    }
+    return render(request,"result.html",context)
+
+def dashboard(request):
+    global id
+    m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+    cursor = m.cursor()
+
+    cmd = "select * from users where UserID='{}'".format(id)
+    cursor.execute(cmd)
+    t = tuple(cursor.fetchall())
+    context = {
+        'data' : t
+    }
+    return render(request,"dashboard.html",context)
+
+def jobs(request):
+    global id
+    m = sql.connect(host='localhost', user='root', passwd='anurag',database='iib')
+    cursor = m.cursor()
+
+    cmd = "select * from job"
+    cursor.execute(cmd)
+    t = tuple(cursor.fetchall())
+    context = {
+        'data' : t
+    }
+    return render(request,"jobs.html",context)
